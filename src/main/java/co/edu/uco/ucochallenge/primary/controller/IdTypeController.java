c7b384bfba011255b7309a5c4ab544fa4d86e2d5
Jose Alejandro Valencia <josevalenciahenao6@gmail.com>
2025-10-30 07:20:31 -0500

    Refactor IdType components to use explicit constructors

--- src/main/java/co/edu/uco/ucochallenge/primary/controller/IdTypeController.java
+++ src/main/java/co/edu/uco/ucochallenge/primary/controller/IdTypeController.java
@@ -9,14 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.uco.ucochallenge.idtype.application.dto.IdTypeDTO;
 import co.edu.uco.ucochallenge.idtype.application.interactor.usecase.IdTypeQueryService;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/uco-challenge/api/v1/id-types")
-@RequiredArgsConstructor
 public class IdTypeController {
 
         private final IdTypeQueryService idTypeQueryService;
+
+        public IdTypeController(final IdTypeQueryService idTypeQueryService) {
+                this.idTypeQueryService = idTypeQueryService;
+        }
 
         @GetMapping
         public ResponseEntity<List<IdTypeDTO>> getAll() {
