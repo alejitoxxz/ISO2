a1b9ea770e1c757feceb6bba700a20e290d95571
Jose Alejandro Valencia <josevalenciahenao6@gmail.com>
2025-10-31 18:51:53 -0500

    Refactor register user use case dependencies

--- src/test/java/co/edu/uco/ucochallenge/user/registeruser/application/interactor/usecase/impl/RegisterUserUseCaseImplTest.java
+++ src/test/java/co/edu/uco/ucochallenge/user/registeruser/application/interactor/usecase/impl/RegisterUserUseCaseImplTest.java
@@ -5,16 +5,20 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import co.edu.uco.ucochallenge.crosscuting.exception.BusinessException;
+import co.edu.uco.ucochallenge.crosscuting.exception.DomainValidationException;
 import co.edu.uco.ucochallenge.crosscuting.helper.UUIDHelper;
 import co.edu.uco.ucochallenge.user.registeruser.application.port.ContactConfirmationPort;
+import co.edu.uco.ucochallenge.user.registeruser.application.port.IdTypeQueryPort;
+import co.edu.uco.ucochallenge.user.registeruser.application.port.LocationQueryPort;
 import co.edu.uco.ucochallenge.user.registeruser.application.port.NotificationPort;
 import co.edu.uco.ucochallenge.user.registeruser.application.port.RegisterUserRepositoryPort;
 import co.edu.uco.ucochallenge.user.registeruser.application.usecase.domain.RegisterUserDomain;
@@ -28,11 +32,18 @@ class RegisterUserUseCaseImplTest {
         private NotificationPort notificationPort;
         @Mock
         private ContactConfirmationPort contactConfirmationPort;
+        @Mock
+        private IdTypeQueryPort idTypeQueryPort;
+        @Mock
+        private LocationQueryPort locationQueryPort;
+        @Mock
+        private Supplier<UUID> idGenerator;
+
+        @InjectMocks
+        private RegisterUserUseCaseImpl useCase;
 
         @Test
-        void shouldThrowBusinessExceptionWhenDomainValidationFails() {
-                var useCase = new RegisterUserUseCaseImpl(repositoryPort, notificationPort, contactConfirmationPort);
-
+        void shouldThrowDomainValidationExceptionWhenDomainValidationFails() {
                 var domain = RegisterUserDomain.builder()
                                 .id(UUID.randomUUID())
                                 .idType(UUIDHelper.getDefault())
@@ -44,7 +55,7 @@ class RegisterUserUseCaseImplTest {
                                 .mobileNumber("")
                                 .build();
 
-                assertThrows(BusinessException.class, () -> useCase.execute(domain));
+                assertThrows(DomainValidationException.class, () -> useCase.execute(domain));
 
                 verify(repositoryPort, never()).save(domain);
                 verify(contactConfirmationPort, never()).confirmEmail(ArgumentMatchers.anyString());
